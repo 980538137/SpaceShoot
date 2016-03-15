493e93e262b475c1842f30777427d1134aa73b9e
Guoxing.song <980538137@qq.com>
2016-03-15 12:15:27 +0800

    maneuver

--- Assets/Scripts/EvasiveManeuver.cs
+++ Assets/Scripts/EvasiveManeuver.cs
@@ -16,10 +16,37 @@ public class EvasiveManeuver : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         currentSpeed = GetComponent<Rigidbody>().velocity.z;
+        StartCoroutine(Evade());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x,startWait.y));
+        while(true)
+        {
+            //如果敌机在x轴正方向，目标值取负值
+            targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+            //等待1-2秒用以机动
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;//目标机动位置归零
+            //等待1-2秒再次机动
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //取得敌机要移动到的目标机动位置
+        float newManeuver = Mathf.MoveTowards(GetComponent<Rigidbody>().velocity.x, targetManeuver, smoothing * Time.deltaTime);
+        GetComponent<Rigidbody>().velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+        GetComponent<Rigidbody>().position = new Vector3(Mathf.Clamp(GetComponent<Rigidbody>().position.x,boundary.xMin,boundary.xMax),0.0f,Mathf.Clamp(GetComponent<Rigidbody>().position.z,boundary.zMin,boundary.zMax));
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0,0,GetComponent<Rigidbody>().velocity.x * -tilt);
+    
+    }
 }
